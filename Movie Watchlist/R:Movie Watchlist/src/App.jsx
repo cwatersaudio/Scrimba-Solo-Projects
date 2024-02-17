c2f14c9d5b0414803ef5e530e7a85808509fb6bd
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-02-17 00:44:14 -0500

    functions work but there's a lag

--- Movie Watchlist/R:Movie Watchlist/src/App.jsx
+++ Movie Watchlist/R:Movie Watchlist/src/App.jsx
@@ -4,10 +4,10 @@ import Navbar from "./components/Navbar";
 
 
 
-export function App() {
+export default function App() {
 
 	const [buttonStatus, setButtonStatus] = React.useState("watchlist")
-
+	const [currentMovies, setCurrentMovies] = React.useState([])
 
 	//State: CURRENT MOVIES
 	// STATE: WATCHLIST
@@ -27,12 +27,40 @@ export function App() {
 				console.log(data);
 				if (data.Response) {
 					const movieResults = data.Search.map((item) => item.imdbID);
+					setCurrentMovies(movieResults) //current movies is here a list of IDs
+					getMovieData(currentMovies)
 				} else if (data.Response === false) {
 					throw Error;
 				}
 			})
 			.catch((err) => console.error("error in fetching"));
 	}
+
+
+	async function getMovieData(currentMovies) {
+		console.log('function ran')
+		let fullMovieData = []
+		for (const ID of currentMovies) {
+			const res = await fetch(`http://www.omdbapi.com/?apikey=a8022ea&i=${ID}`);
+			const movData = await res.json()
+			const currentMovie = {
+				title: movData.Title,
+				poster: movData.Poster,
+				imdbRating: movData.imdbRating,
+				runtime: movData.Runtime,
+				genre: movData.Genre,
+				plot: movData.Plot,
+				imdbID: movData.imdbID
+			}
+			fullMovieData.push(currentMovie)
+			console.log(currentMovie)
+		}
+	}
+
+
+
+
+
 	return (
 		<>
 			<Navbar
@@ -44,4 +72,3 @@ export function App() {
 	);
 }
 
-export default App;
