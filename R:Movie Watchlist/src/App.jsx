5a0133334a557c029f8263f0d0eaf3c292a51e4e
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-03-10 23:54:19 -0400

    initial commit

--- R:Movie Watchlist/src/App.jsx
+++ R:Movie Watchlist/src/App.jsx
@@ -20,23 +20,27 @@ export default function App() {
 
 	// clearMovieArea();
 	//gets an array of movies
-
+	React.useState
 
 	function searchMovie(title) {
-		console.log(title)
+		if (title.length > 0) {
+			fetch(`http://www.omdbapi.com/?apikey=a8022ea&s=${title}`)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+					if (data.Response) {
+						const movieResults = data.Search.map((item) => item.imdbID);
+						getMovieData(movieResults)
+					} else if (data.Response === false) {
+						throw Error;
+					}
+				})
+				.catch((err) => {
+					console.error("error in fetching")
 
-		fetch(`http://www.omdbapi.com/?apikey=a8022ea&s=${title}`)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				if (data.Response) {
-					const movieResults = data.Search.map((item) => item.imdbID);
-					getMovieData(movieResults)
-				} else if (data.Response === false) {
-					throw Error;
 				}
-			})
-			.catch((err) => console.error("error in fetching"));
+				);
+		}
 	}
 
 
@@ -95,6 +99,9 @@ export default function App() {
 
 
 
+
+
+
 	return (
 		<div className="app--container">
 			<Navbar
@@ -108,19 +115,21 @@ export default function App() {
 					removeFromWatchlist={removeFromWatchlist}
 				/>
 
-				{/* // (currentMovies.length > 0) ?
-				// 	<SearchDisplay
-				// 		currentMovies={currentMovies}
-				// 	/>
-					// :
-					// <div className="no--movies">
-					// 	<img
-					// 		src={movieIcon}
-					// 		alt="movie icon"
-					// 	/>
-					// 	<p className="dafault--text">Start Exploring</p>
-					// </div>
-				// } */}
+				{(currentMovies.length > 0) ?
+					<SearchDisplay
+						currentMovies={currentMovies}
+						addToWatchlist={addToWatchlist}
+						removeFromWatchlist={removeFromWatchlist}
+					/>
+					:
+					<div className="no--movies">
+						<img
+							src={movieIcon}
+							alt="movie icon"
+						/>
+						<p className="dafault--text">Start Exploring</p>
+					</div>
+				}
 
 			</div>
 
